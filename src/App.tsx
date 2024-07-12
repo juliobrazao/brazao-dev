b2c20991702a60146296efc2c27938832ac49e93
Julio Brazao <jotabrazao@gmail.com>
2024-07-11 22:39:34 -0400

    Basic Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
 export default function App() {
-  return <>My App</>;
+  return (
+    <>
+      <div className="d-flex justify-content-center align-items-center vh-100">
+        <div className="container-fluid text-center">
+          julio brazao - <strong>software developer</strong>
+          <h2 className="mt-3">
+            <strong>julio@brazao.dev</strong>
+          </h2>
+        </div>
+      </div>
+    </>
+  );
 }
