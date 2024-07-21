8fdd7a0198c77b4a1a671ac76243fa5c0a979ea0
Julio Brazao <jotabrazao@gmail.com>
2024-07-21 18:19:45 -0400

    Customizing home

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ export default function App() {
   return (
     <>
       <div className="d-flex justify-content-center align-items-center vh-100">
-        <h1>julio@brazao.dev</h1>
+        <h1>
+          julio@<strong>brazao</strong>.dev
+        </h1>
       </div>
     </>
   );
